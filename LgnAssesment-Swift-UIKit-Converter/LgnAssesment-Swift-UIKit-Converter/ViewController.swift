5c2b85a88cc9b8b46b0a9b66c71cf9bc00a52a19
legionpro <legion4reg@gmail.com>
2025-01-19 14:52:54 +0200

    project  is just ceated in xcode

---
+++ LgnAssesment-Swift-UIKit-Converter/LgnAssesment-Swift-UIKit-Converter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LgnAssesment-Swift-UIKit-Converter
+//
+//  Created by Oleh Poremskyy on 19.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
